111b03669a9dcc166ff25b609c47550b92b9c67e
jfonz412 <jfonzone@outlook.com>
2017-04-29 13:44:31 -0400

    refactored bianry tree, dfs still not working...

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,57 +1,42 @@
+# REFACTORED UNTIL ===
+
 class Node
   attr_reader :left_child, :right_child, :value
 
-  def initialize(value)
+  def initialize(value = nil)
   	@value = value
   	@left_child = nil
   	@right_child = nil
   end
 
-  def build_tree(array)
-    return if array.empty?
-
-    left_side = []
-    right_side = []
-
-    array.each do |i|
-      if i <= @value
-      	left_side << i
-      else
-      	right_side << i
-      end
+  def add_node(value)
+    if @value.nil?
+      @value = value
+    elsif value <= @value
+      @left_child.add_node(value) if @left_child != nil
+      @left_child = Node.new(value) if @left_child == nil
+    else
+      @right_child.add_node(value) if @right_child != nil
+      @right_child = Node.new(value) if @right_child == nil
     end
-    
-    if left_side.empty? == false
-      @left_child = Node.new(left_side[0])
-      left_side.shift
-      @left_child.build_tree(left_side) 
-    end
-    
-    if right_side.empty? == false
-      @right_child = Node.new(right_side[0])
-      right_side.shift
-      @right_child.build_tree(right_side)
-    end
-    
   end
 
-  def add_node(new_value)
-
-  	if new_value <= @value
-  	  if @left_child == nil
-  	  	@left_child = Node.new(new_value) 
-  	  else
-  	    @left_child.add_node(new_value)
-  	  end
-  	else
-  	  if @right_child == nil
-        @right_child = Node.new(new_value)
-      else
-  	    @right_child.add_node(new_value)
-  	  end
-  	end
-  end
 end
+
+def build_tree(array)
+  root_node = Node.new
+  array.each {|i| root_node.add_node(i)}
+  return root_node
+end
+
+def print_tree(root)
+  puts "Value: #{root.value}"
+  puts "Left Child: #{root.left_child.value}"
+  puts "Right Child #{root.right_child.value}"
+end
+
+#======================================================
+
 
 def breadth_first_search(root,target)
   queue = [] #FIFO
@@ -69,34 +54,30 @@ def breadth_first_search(root,target)
   end
 end
 
-def depth_first_search(root,target) 
-  return nil if root.nil?
-  #return root if root.value == target
-  p root if root.value == target
+#NO IDEA WHY THIS DOESN'T WORK. IT PRINTS THE CORRECT NODE BUT ONLY RETURNS NIL
+def depth_first_search(node,target) 
+  return nil if node.nil?
+  p node if node.value == target
 
-  left = root.left_child
-  right = root.right_child
+  left = node.left_child
+  right = node.right_child
   
-  depth_first_search(left, target) unless left.nil?
-  depth_first_search(right, target) unless right.nil?
+  left_results = depth_first_search(left, target) unless left.nil?
+  right_results = depth_first_search(right, target) unless right.nil?
 end
 
 #RANDOMIZED ARRAY
+=begin
 array = []
 20.times do
   array << 1 +rand(20)
 end
+=end
 
-#array = [7,2,18,6,15,11,12,20,19,3,1,9,5,13,17,14,16,8,4,10]
+array = [7,2,18,6,15,11,12,20,19,3,1,9,5,13,17,14,16,8,4,10]
 
-
-root_node = Node.new(12)
-
-root_node.build_tree(array)
-root_node.add_node(7)
-
-breadth_first_search(root_node,3)
-
-p depth_first_search(root_node,3) #seems to print the nde when found, but won't return it
-									#might be a recursion issue...
+root = build_tree(array)
+print_tree(root)
+p depth_first_search(root, 3)
+p breadth_first_search(root,3)
 
